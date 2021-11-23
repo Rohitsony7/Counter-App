a0f97f7dc89d6f7ad0f0797cb51a3acd0f386032
Rohit Soni <Rsony.721@gmail.com>
2021-11-23 13:06:43 +0530

    Counter - App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter-App';
+  currentCount: number = 0;
+
+  resetseMe = () => {
+    this.currentCount = 0;
+  };
+
+  increaseMe = () => {
+    if (this.currentCount >= 10) {
+      alert('Current count upper limit exceeded.');
+    } else {
+      this.currentCount++;
+    }
+  };
+
+  decreaseMe = () => {
+    if (this.currentCount <= 0) {
+      alert('Current count lower limit exceeded.');
+    } else {
+      this.currentCount--;
+    }
+  };
 }
